ff7d38aaa0740337dcb7297f11f9c379813db01a
Kulpreet Singh <kulpreetsingh.acme@gmail.com>
2022-02-01 22:49:38 -0800

    fix module

--- main.tf
+++ main.tf
@@ -9,5 +9,4 @@ module "GET-SECRETID-B-APPROLE" {
   depends_on = [module.GET-TOKEN-VIA-APPROLE]
   source = "./approle-b"
   secret_id_b = module.GET-TOKEN-VIA-APPROLE.secret_id_b
-  role_id_b = var.role_id_b
 }
