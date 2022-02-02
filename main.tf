2bad2d7ef345bff2d8ca60f49912708e89071a6a
Kulpreet Singh <kulpreetsingh.acme@gmail.com>
2022-02-01 22:52:49 -0800

    fix module

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
-module "GET-TOKEN-VIA-APPROLE" {
-  source = "./token_role_seeding_approle_a"
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.2.1"
+    }
+  }
 }
 
+resource "vault_approle_auth_backend_login" "login" {
+  role_id   = var.role_id_a
+  secret_id = var.secret_id_a
+}
 
-module "GET-SECRETID-B-APPROLE" {
-  depends_on = [module.GET-TOKEN-VIA-APPROLE]
-  source = "./approle-b"
-  secret_id_b = module.GET-TOKEN-VIA-APPROLE.secret_id_b
+output "token-a" {
+  value = vault_approle_auth_backend_login.login
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "id_b" {
+  role_name = "approle-b"
+}
+
+output "secret_id_b" {
+  value = vault_approle_auth_backend_role_secret_id.id_b.secret_id
 }
